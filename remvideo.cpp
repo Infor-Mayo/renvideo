e1aa80094e243ec9af8b5548e2c2a9cf78153dd8
Infor_Mayo <106414650+Infor-Mayo@users.noreply.github.com>
2024-09-19 16:27:53 -0400

    19/9/24

--- remvideo.cpp
+++ remvideo.cpp
@@ -9,8 +9,10 @@
 RemVideo::RemVideo(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::RemVideo)
+    , lastLogTime(QDateTime())  // Inicializar con un valor nulo
 {
     ui->setupUi(this);
+    ui->noe->setChecked(true);
 }
 
 RemVideo::~RemVideo()
@@ -20,150 +22,244 @@ RemVideo::~RemVideo()
 
 void RemVideo::on_selectFolderButton_clicked()
 {
-    QString currentFolder = QFileDialog::getExistingDirectory(this, "Select Folder");
+    QString currentFolder = QFileDialog::getExistingDirectory(this, tr("Seleccionar Carpeta"));
     if (!currentFolder.isEmpty()) {
         ui->carpeta->setText(currentFolder);
     }
 }
 
-void RemVideo::renameVideosInFolder(const QString& folderPath, int seasonNumber, const QString& customName)
+void RemVideo::renameVideosInFolder(const QString& folderPath, int seasonNumber, const QString& customName, bool deleteImages, bool deleteNfoFiles, bool deleteLog)
 {
-    // Extensiones de archivos de video permitidas
-    QStringList videoExtensions = {"*.mp4", "*.mkv", "*.avi", "*.mov", "*.flv"};
+    const QStringList videoExtensions = {"*.mp4", "*.mkv", "*.avi", "*.mov", "*.flv", "*.ts"};
+    const QStringList imageExtensions = {"*.jpg", "*.jpeg", "*.png", "*.gif", "*.bmp"};
+    QDirIterator it(folderPath, videoExtensions + (deleteImages ? imageExtensions : QStringList()) + (deleteNfoFiles ? QStringList("*.nfo") : QStringList()), QDir::Files | QDir::NoSymLinks, QDirIterator::Subdirectories);
+    QRegularExpression re("(\\d+)");
 
-    // Iterar sobre los archivos de video en la carpeta actual y sus subdirectorios
-    QDirIterator it(folderPath, videoExtensions, QDir::Files | QDir::NoSymLinks, QDirIterator::Subdirectories);
+    QString logFilePath = folderPath + "/rename_log.txt";
+    QFile logFile(logFilePath);
+    if (!logFile.open(QIODevice::Append | QIODevice::Text)) {
+        appendToLog(QString("-e- No se pudo abrir el archivo de log"));
+        return;
+    }
+    QTextStream logStream(&logFile);
 
     while (it.hasNext()) {
-        QString filePath = it.next();
-        QFileInfo fileInfo(filePath);
+        QFileInfo fileInfo(it.next());
+        QString suffix = fileInfo.suffix().toLower();
+        
+        // Eliminar imágenes si deleteImages es verdadero
+        if (deleteImages && imageExtensions.contains("*." + suffix)) {
+            if (QFile::remove(fileInfo.filePath())) {
+                QString logEntry = QString("-d- Imagen eliminada: %1").arg(fileInfo.fileName());
+                appendToLog(logEntry);
+                logStream << logEntry << "\n";
+            } else {
+                QString logEntry = QString("-e- Error al eliminar imagen: %1").arg(fileInfo.fileName());
+                appendToLog(logEntry);
+                logStream << logEntry << "\n";
+            }
+            continue;
+        }
+        
+        // Eliminar archivos .nfo si deleteNfoFiles es verdadero
+        if (deleteNfoFiles && suffix == "nfo") {
+            if (QFile::remove(fileInfo.filePath())) {
+                QString logEntry = QString("-d- Archivo NFO eliminado: %1").arg(fileInfo.fileName());
+                appendToLog(logEntry);
+                logStream << logEntry << "\n";
+            } else {
+                QString logEntry = QString("-e- Error al eliminar archivo NFO: %1").arg(fileInfo.fileName());
+                appendToLog(logEntry);
+                logStream << logEntry << "\n";
+            }
+            continue;
+        }
 
+        // Continuar con el renombrado de archivos de video
         QString baseName = fileInfo.completeBaseName();
-        QRegularExpression re("(\\d+)");
-        QRegularExpressionMatchIterator matchIt = re.globalMatch(baseName);
+        auto matchIt = re.globalMatch(baseName);
 
-        QString chapterNumber = "01"; // Número de capítulo por defecto
-        QString detectedSeason = (seasonNumber > 0) ? QString::number(seasonNumber) : "01"; // Temporada ingresada o "01"
+        QString chapterNumber = "01";
+        QString detectedSeason = seasonNumber > 0 ? QString::number(seasonNumber) : "01";
 
         if (matchIt.hasNext()) {
-            // Primer número detectado en el nombre
-            QRegularExpressionMatch match = matchIt.next();
+            auto match = matchIt.next();
             if (seasonNumber == 0) {
-                detectedSeason = match.captured(1); // Si no se ingresó temporada manual, usar el primer número
+                detectedSeason = match.captured(1);
             }
 
             if (matchIt.hasNext()) {
-                // Segundo número (capítulo)
-                match = matchIt.next();
-                chapterNumber = match.captured(1); // Usar segundo número como capítulo
+                chapterNumber = matchIt.next().captured(1);
             } else {
-                // Si solo hay un número, este es el capítulo
                 chapterNumber = detectedSeason;
-                detectedSeason = (seasonNumber > 0) ? QString::number(seasonNumber) : "01"; // Usar temporada ingresada o "01"
+                detectedSeason = seasonNumber > 0 ? QString::number(seasonNumber) : "01";
             }
         }
 
-        // Formato del nombre final
         QString finalName = QString("capítulo %1x%2 (%3).%4")
                                 .arg(detectedSeason, 2, '0')
                                 .arg(chapterNumber, 2, '0')
-                                .arg(!customName.isEmpty() ? customName : fileInfo.dir().dirName()) // Nombre personalizado o carpeta contenedora
+                                .arg(!customName.isEmpty() ? customName : fileInfo.dir().dirName())
                                 .arg(fileInfo.suffix());
 
         QString newFilePath = fileInfo.absolutePath() + "/" + finalName;
 
-        if (QFile::rename(filePath, newFilePath)) { // Renombrar el archivo
-            QString logEntry = QString("-n- %1 -> %2").arg(fileInfo.fileName()).arg(finalName);
-
-            // Crear archivo de registro en la carpeta del archivo renombrado
-            QString logFilePath = fileInfo.absolutePath() + "/rename_log.txt"; // Archivo de log individual
-            QFile logFile(logFilePath);
-
-            // Abrir el archivo de registro en modo de anexado
-            if (logFile.open(QIODevice::Append | QIODevice::Text)) {
-                QTextStream logStream(&logFile);
-                logStream << logEntry << "\n"; // Guardar el renombre en el archivo de registro
-                logFile.close();
+        if (fileInfo.filePath() != newFilePath) {
+            if (QFile::rename(fileInfo.filePath(), newFilePath)) {
+                QString logEntry = QString("-n- %1 -> %2").arg(fileInfo.fileName(), finalName);
+                appendToLog(logEntry);
+                logStream << logEntry << "\n";
+            } else {
+                QString logEntry = QString("-e- Error renaming: %1").arg(fileInfo.fileName());
+                appendToLog(logEntry);
+                logStream << logEntry << "\n";
             }
-
-            appendToLog(logEntry);  // Mostrar en la interfaz
         } else {
-            QString errorLogEntry = QString("-e- Error renaming: %1").arg(fileInfo.fileName());
-            appendToLog(errorLogEntry);
+            QString logEntry = QString("-i- El archivo ya tiene el nombre correcto: %1").arg(fileInfo.fileName());
+            appendToLog(logEntry);
+            logStream << logEntry << "\n";
+        }
+    }
+
+    logFile.close();
+
+    // Eliminar el archivo de log si deleteLog es verdadero
+    if (deleteLog) {
+        if (QFile::remove(logFilePath)) {
+            QString logEntry = QString("-d- Archivo de log eliminado: %1").arg(logFilePath);
+            appendToLog(logEntry);
+        } else {
+            QString logEntry = QString("-e- Error al eliminar archivo de log: %1").arg(logFilePath);
+            appendToLog(logEntry);
         }
     }
 }
 
 void RemVideo::restoreVideosInFolder(const QString& folderPath)
 {
-    // Iterar sobre todas las subcarpetas y archivos de registro
+    QFile globalLogFile(QCoreApplication::applicationDirPath() + "/global_log.txt");
+    if (!globalLogFile.open(QIODevice::Append | QIODevice::Text)) {
+        appendToLog(QString("-e- No se pudo abrir el archivo de log global"));
+        return;
+    }
+    QTextStream globalLogStream(&globalLogFile);
+
     QDirIterator it(folderPath, QDirIterator::Subdirectories);
     while (it.hasNext()) {
         QString currentFolder = it.next();
-        QString logFilePath = currentFolder + "/rename_log.txt";
-        QFile logFile(logFilePath);
+        QFile logFile(currentFolder + "/rename_log.txt");
 
         if (logFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
             QTextStream logStream(&logFile);
-            QStringList logEntries = logStream.readAll().split("\n", Qt::SkipEmptyParts);
-
-            // Buscar la última línea que comienza con -n-
             QString lastRenameEntry;
-            for (const QString &entry : logEntries) {
-                if (entry.startsWith("-n-")) {
-                    lastRenameEntry = entry; // Guardar la última entrada de renombre
+
+            while (!logStream.atEnd()) {
+                QString line = logStream.readLine();
+                if (line.startsWith("-n-")) {
+                    lastRenameEntry = line;
                 }
             }
 
-            // Si se encontró una entrada de renombre, proceder a restaurar
             if (!lastRenameEntry.isEmpty()) {
                 QStringList parts = lastRenameEntry.split(" -> ");
                 if (parts.size() == 2) {
-                    QString originalFileName = parts.at(0).mid(4); // Eliminar el prefijo -n-
+                    QString originalFileName = parts.at(0).mid(4);
                     QString newFileName = parts.at(1);
                     QString originalFilePath = currentFolder + "/" + originalFileName;
                     QString newFilePath = currentFolder + "/" + newFileName;
 
-                    // Verificar si el nuevo archivo existe antes de renombrar
                     if (QFile::exists(newFilePath)) {
                         if (QFile::rename(newFilePath, originalFilePath)) {
-                            QString restoreLogEntry = QString("-r- %1 -> %2").arg(newFileName).arg(originalFileName);
-                            appendToLog(restoreLogEntry);
+                            QString logEntry = QString("-r- %1 -> %2").arg(newFileName, originalFileName);
+                            appendToLog(logEntry);
+                            globalLogStream << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " - " << logEntry << "\n";
                         } else {
-                            QString errorLogEntry = QString("-e- Error restoring: %1").arg(newFileName);
-                            appendToLog(errorLogEntry);
+                            QString logEntry = QString("-e- Error restoring: %1").arg(newFileName);
+                            appendToLog(logEntry);
+                            globalLogStream << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " - " << logEntry << "\n";
                         }
                     } else {
-                        appendToLog(QString("File not found for restoration: %1").arg(newFileName));
+                        QString logEntry = QString("File not found for restoration: %1").arg(newFileName);
+                        appendToLog(logEntry);
+                        globalLogStream << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " - " << logEntry << "\n";
                     }
                 }
             }
-            logFile.close();
         }
     }
+
+    globalLogFile.close();
 }
 
 void RemVideo::appendToLog(const QString& logText)
 {
-    ui->logTextEdit->append(logText); // Mostrar en la interfaz
+    QDateTime currentTime = QDateTime::currentDateTime();
+    QString logFilePath = QCoreApplication::applicationDirPath() + "/global_log.txt";
+    QFile globalLogFile(logFilePath);
+    
+    if (globalLogFile.open(QIODevice::Append | QIODevice::Text)) {
+        QTextStream globalLogStream(&globalLogFile);
+
+        // Agregar el separador al archivo de log si han pasado más de 10 segundos
+        if (lastLogTime.isNull() || lastLogTime.secsTo(currentTime) > 10) {
+            QString separator = QString("------ [%1] --------\n").arg(currentTime.toString("yyyy-MM-dd hh:mm:ss"));
+            globalLogStream << separator;
+            lastLogTime = currentTime;
+        }
+
+        // Agregar el mensaje de log al archivo
+        globalLogStream << logText << "\n";
+
+        globalLogFile.close();
+    } else {
+        qDebug() << "No se pudo abrir el archivo de log:" << logFilePath;
+    }
+
+    // Agregar solo el mensaje de log a la interfaz de usuario
+    ui->logTextEdit->append(logText);
 }
 
 void RemVideo::on_renombrar_clicked()
 {
     QString folderPath = ui->carpeta->text();
-    QString customName = ui->nombre->text();
-    int seasonNumber = ui->temporada->value(); // Valor ingresado de temporada
-
     if (!folderPath.isEmpty()) {
-        renameVideosInFolder(folderPath, seasonNumber, customName); // Llamada a renombrar videos
+        renameVideosInFolder(folderPath, ui->temporada->value(), ui->nombre->text(), ui->eimg->isChecked(), ui->enfo->isChecked(), ui->elog->isChecked());
     }
 }
 
 void RemVideo::on_restaurar_clicked()
 {
     QString folderPath = ui->carpeta->text();
-
     if (!folderPath.isEmpty()) {
-        restoreVideosInFolder(folderPath); // Restaurar nombres originales
+        restoreVideosInFolder(folderPath);
     }
 }
+
+void RemVideo::on_enfo_clicked(bool checked)
+{
+    if (checked){
+        ui->noe->setChecked(false);
+    }else if(!checked&&!ui->eimg->isChecked()&&!ui->elog->isChecked()){
+        ui->noe->setChecked(true);
+    }
+
+}
+
+void RemVideo::on_eimg_clicked(bool checked)
+{
+    if (checked){
+        ui->noe->setChecked(false);
+    }else if(!checked&&!ui->enfo->isChecked()&&!ui->elog->isChecked()){
+        ui->noe->setChecked(true);
+    }
+}
+
+void RemVideo::on_elog_clicked(bool checked)
+{
+    if (checked){
+        ui->noe->setChecked(false);
+    }else if(!checked&&!ui->enfo->isChecked()&&!ui->eimg->isChecked()){
+        ui->noe->setChecked(true);
+    }
+}
+
--- remvideo.h
+++ remvideo.h
@@ -8,6 +8,8 @@
 #include <QTextStream>
 #include <QSpinBox>
 #include <QTextEdit>
+#include <QDateTime>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class RemVideo; }
@@ -25,12 +27,18 @@ private slots:
     void on_selectFolderButton_clicked();
     void on_renombrar_clicked();
     void on_restaurar_clicked();
+    void on_enfo_clicked(bool checked);
+    void on_eimg_clicked(bool checked);
+    void on_elog_clicked(bool checked);
 
 private:
     Ui::RemVideo *ui;
-    void renameVideosInFolder(const QString& folderPath, int seasonNumber, const QString& customName);
+    void renameVideosInFolder(const QString& folderPath, int seasonNumber, const QString& customName, bool deleteImages, bool deleteNfoFiles, bool deleteLog);
     void restoreVideosInFolder(const QString& folderPath);
     void appendToLog(const QString& logText);
+
+    QDateTime lastLogTime;
+    const int batchTimeThresholdMs = 1000; // 1 segundo de umbral para considerar registros como lote
 };
 
 #endif // REMVIDEO_H
